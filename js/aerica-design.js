6b5ac38e783995446344cb2d7fac752d96d29b64
aerica <aericalovett16@gmail.com>
2017-09-18 20:06:31 -0400

    add extra pages
    adding the contact and about pages

--- js/aerica-design.js
+++ js/aerica-design.js
@@ -1,13 +1,14 @@
 $(function() {
-	function changegrayscale(value) {
-	  	$('.gallery-img-cont').css({'filter': 'grayscale('+value+'%)'});
-	  }
-	
+	var galleryImgCont = $('.gallery-img-cont');
 	var galleryItem = $('#gallery * .gallery-item-cont');
 	var galleryItemLink = $('#gallery * .gallery-btn-cont');
 	// alert(galleryItem.length);
 	
 	$(galleryItem).each(function(x) {
+		function changegrayscale(value) {
+	  	$(galleryImgCont[x]).css({'filter': 'grayscale('+value+'%)'});
+	  }
+		
 		$(this).hover(function() {
 			$(galleryItemLink[x]).css('background-color', 'rgba(0,0,0,0.0)');
 			  changegrayscale('0');
